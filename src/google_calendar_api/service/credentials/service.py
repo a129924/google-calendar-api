c7a8cf4c3578b8a6c842860c13b5cd1b69db65e1
a129924 <a129924@gmail.com>
2024-07-16 16:43:45 +0800

    doc: 新增文件

--- src/google_calendar_api/service/credentials/service.py
+++ src/google_calendar_api/service/credentials/service.py
@@ -9,6 +9,16 @@ class CredentialsService:
         self.token_json_path = token_json_path
 
     def get_service(self, service_name: str, version: str) -> Resource:
+        """
+        get_service 返回指定的 Google API 服務對象，並在需要時刷新憑證。
+
+        Args:
+            service_name (str): Google API 服務的名稱（例如 "calendar"）。
+            version (str): Google API 服務的版本（例如 "v3"）。
+
+        Returns:
+            CredentialService: 指定的 Google API 服務對象。
+        """
         if self.credentials.need_refresh():
             self.credentials.refresh_token()
 
@@ -29,6 +39,20 @@ class CredentialsService:
         client_secret: str,
         scopes: list[str],
     ) -> "CredentialsService":
+        """
+        from_token_params 從提供的 token 參數生成 CredentialService 實例。
+
+        Args:
+            token (str): 訪問 token。
+            refresh_token (str): 刷新 token。
+            token_uri (str): token URI。
+            client_id (str): 客戶端 ID。
+            client_secret (str): 客戶端密鑰。
+            scopes (list[str]): 授權範圍。
+
+        Returns:
+            CredentialsService: 生成的 CredentialService 實例。
+        """
         creds = Credentials(
             token=token,
             refresh_token=refresh_token,
@@ -48,6 +72,18 @@ class CredentialsService:
         *,
         output_token_json: str,
     ) -> "CredentialsService":
+        """
+        from_client_secrets_file 產生google service 必要的client secrets file
+
+        Args:
+            credentials_json_path (str): client secrets filepath
+            scopes (list[str]): 服務網址
+            output_token_json (str): 憑證刷新後將新的憑證保存到這個路徑（如果提供）。
+            port (int, optional): port號. Defaults to 0.
+
+        Returns:
+            CredentialsService: 生成的 CredentialService 實例。
+        """
         creds = Credentials.from_client_secrets_file(
             credentials_json_path, scopes, port
         )
@@ -58,6 +94,16 @@ class CredentialsService:
     def from_authorized_user_file(
         token_json_path: str, scopes: list[str]
     ) -> "CredentialsService":
+        """
+        from_authorized_user_file 產生google service 必要的token path
+
+        Args:
+            token_json_path (str): 憑證刷新後將新的憑證保存到這個路徑（如果提供）。
+            scopes (list[str]): 服務網址
+
+        Returns:
+            CredentialsService: 生成的 CredentialService 實例。
+        """
         creds = Credentials.from_authorized_user_file(token_json_path, scopes)
 
         return CredentialsService(creds, token_json_path)
