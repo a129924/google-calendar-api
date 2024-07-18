d7a04ab95bcda70db09963b5848344f75e8c8cfd
a129924 <a129924@gmail.com>
2024-07-18 16:30:51 +0800

    feat : 新增對application 更嚴格的event param

--- src/google_calendar_api/types/calendar.py
+++ src/google_calendar_api/types/calendar.py
@@ -1,8 +1,8 @@
-from typing_extensions import TypedDict
+from typing_extensions import NotRequired, Required, TypedDict
 
 from ..schema.calendar import Attendee, Reminders
 
-__all__ = ["EventParam"]
+__all__ = ["EventParam", "ApplicationAddEventParam"]
 
 
 class EventParam(TypedDict, total=False):
@@ -14,3 +14,13 @@ class EventParam(TypedDict, total=False):
     attendees: list[Attendee] | None
     reminders: Reminders | None
     time_zone: str | None
+
+
+class ApplicationAddEventParam(TypedDict, total=False):
+    summary: Required[str]
+    start_time: Required[str]
+    end_time: Required[str]
+    location: Required[str]
+    description: Required[str | None]
+    attendees: NotRequired[list[Attendee] | None]
+    reminders: NotRequired[Reminders | None]
