134d42c4b27c0fccac073465ea9e728ebac52557
a129924 <a129924@gmail.com>
2024-07-18 16:32:28 +0800

    feat : 新增CalendarConfig 負責讀取有資安風險的config

--- src/google_calendar_api/config/config.py
+++ src/google_calendar_api/config/config.py
@@ -1,4 +1,22 @@
+from pydantic import BaseModel
+
 from .base import JsonBaseSettings
+
+__all__ = ["CalendarConfig"]
+
+
+class Installed(BaseModel):
+    client_id: str
+    project_id: str
+    auth_uri: str
+    token_uri: str
+    auth_provider_x509_cert_url: str
+    client_secret: str
+    redirect_uris: list[str]
+
+
+class Credentials(JsonBaseSettings):
+    installed: Installed
 
 
 class AuthorizedUserToken(JsonBaseSettings):
@@ -11,3 +29,8 @@ class AuthorizedUserToken(JsonBaseSettings):
     universe_domain: str
     account: str
     expiry: str
+
+
+class CalendarConfig(JsonBaseSettings):
+    calendar_id: str
+    time_zone: str
